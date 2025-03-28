6543c7cab7ca7d625e5f7b77b8af9317487067f1
Matthew Sorensen <a02436822@usu.edu>
2025-03-27 19:28:12 -0600

    lil writing

--- src/main.py
+++ src/main.py
@@ -1,5 +1,27 @@
+import sys
+
 def main():
-    print("Hello World")
+    #Welcome the user to the program
+    print("Flashcards!")
+    menu()
+
+
+def menu():
+    user_input = input("CSV to flashcards (c)\nNew flashcard set (n)\nQuit application (q)")
+    if user_input[0].lower() == 'q':
+        exit(0)
+    elif user_input[0].lower() == 'c':
+        csv_to_dict()
+    elif user_input[0].lower() == 'n':
+        #TODO: new flashcards
+        return
+    else:
+        print("Unexpected input: " + user_input, file=sys.stderr)
+        exit(1)
+
+def csv_to_dict():
+    return
+
 
 if __name__ == '__main__':
     main()
