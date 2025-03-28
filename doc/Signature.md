8224acdabf7a5b1aa46a4922d4bc8a86db3eff6a
Matthew Sorensen <a02436822@usu.edu>
2025-03-28 16:56:36 -0600

    csv to dict function

--- doc/Signature.md
+++ doc/Signature.md
@@ -1,1 +1,2 @@
 March 26 : Creation & Formatting of repo.
+March 28: csv_to_dict()
--- src/main.py
+++ src/main.py
@@ -3,15 +3,16 @@ import sys
 def main():
     #Welcome the user to the program
     print("Flashcards!")
-    menu()
+    flashcards = menu()
+    print(flashcards)
 
 
 def menu():
-    user_input = input("CSV to flashcards (c)\nNew flashcard set (n)\nQuit application (q)")
+    user_input = input("CSV to flashcards (c)\nNew flashcard set (n)\nQuit application (q)\n\t> ")
     if user_input[0].lower() == 'q':
         exit(0)
     elif user_input[0].lower() == 'c':
-        csv_to_dict()
+        return csv_to_dict()
     elif user_input[0].lower() == 'n':
         #TODO: new flashcards
         return
@@ -20,7 +21,25 @@ def menu():
         exit(1)
 
 def csv_to_dict():
-    return
+    """
+    Ask user for csv file location, splits each line of file into 2 parts.
+
+    Uses the first part of the split line as the key to each dictionary entry.
+    returns dictionary.
+    """
+    #Ask user for file location
+    file_location = input("Please enter the exact file location of your csv file:\n\t> ")
+    file = open(file_location)
+    temp = {}
+    for line in file:
+        if ',' not in line:
+            continue
+        line = (line.split(',', 1))
+        temp[line[0]] = line[1].strip('\n')
+
+    file.close()
+    return temp
+
 
 
 if __name__ == '__main__':
